fa26151b1d0cb109a4d59e8686304ba190ea2d90
Maxwell Lasky <mlasky46@gmail.com>
2019-11-20 21:11:58 -0700

    Adds the about page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,7 @@
 import React from "react";
 
-// import { PreviewLogo } from "../navigation/Navigation";
-// import twitterIcon from "../../images/social-icons/twitter-social.svg";
-// import githubIcon from "../../images/social-icons/github-social.svg";
 import facebookIcon from "../images/facebook-social.svg";
 import youtubeIcon from "../images/youtube-social.svg";
-// import "./Footer.scss";
 
 export default () => {
   return (
@@ -36,31 +32,6 @@ export default () => {
             </a>
           </div>
         </div>
-        {/* <PreviewLogo />
-      </div>
-      <span className="footer-flex-container">© 2019 City Of Zion</span>
-      <div id="footer-social-icons" className="footer-flex-container">
-        <a
-          href="https://twitter.com/coz_official"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img alt="twitter-icon" src={twitterIcon} />
-        </a>
-        <a
-          href="https://github.com/CityOfZion"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img alt="github-icon" src={githubIcon} />
-        </a>
-        <a
-          href="https://join.slack.com/t/cityofzion/shared_invite/enQtMjcwOTUxNDc1ODU5LWFhNzY1NWEyZTgyNWUyMTJiY2UwZTI3ZjlmMmI1ZDI1NDU1M2U0N2NkYjc2MjhhZGEyY2Q5YjJlZjQyYTkxMzU"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img alt="slack-icon" src={slackIcon} />
-        </a> */}
       </div>
     </footer>
   );
