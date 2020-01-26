5dd91a318c714f6c6cdc33719b0c712f56e6e0f1
Maxwell Lasky <mlasky46@gmail.com>
2020-01-25 21:27:36 -0700

    Updates

---
+++ component---src-pages-404-js-55f62237f6fac408927a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),o=n("Bl7J"),u=n("vrFN");t.default=function(){return l.a.createElement(o.a,null,l.a.createElement(u.a,{title:"404: Not found"}),l.a.createElement("div",{id:"not-found-container"},l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-55f62237f6fac408927a.js.map
